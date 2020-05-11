60660a1c0ac9ad3e12a386134655982b89dfb963
vigneshwaris <vikiraju@gmail.com>
2020-05-11 22:16:20 +0530

    Update prgmassignment3.R

--- prgmassignment3.R
+++ prgmassignment3.R
@@ -1,4 +1,10 @@
+#Caching the Mean of a Vector
+##The first function, makeVector creates a special "vector", which is really a list containing a function to
 
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
 makeVector <- function(x = numeric()) {
   m <- NULL
   set <- function(y) {
@@ -12,6 +18,10 @@ makeVector <- function(x = numeric()) {
        setmean = setmean,
        getmean = getmean)
 }
+#The following function calculates the mean of the special "vector" created 
+#with the above function. However, it first checks to see if the mean has
+#already been calculated. If so, it gets the mean from the cache and 
+#skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cachemean <- function(x, ...) {
   m <- x$getmean()
